1a5da5d0062d3bd4ea04a89623548a18ee3b05b8
YasaswiniY96 <yasaswini.yarragangu@bluebinaries.com>
2025-04-02 12:03:58 +0530

    Refactor and Firebase dependencies

--- app/src/main/java/com/bluebinaries/carrental/ui/CarRentalActivity.java
+++ app/src/main/java/com/bluebinaries/carrental/ui/CarRentalActivity.java
@@ -1,4 +1,4 @@
-package com.example.carrental;
+package com.bluebinaries.carrental.ui;
 
 import android.os.Bundle;
 
@@ -8,7 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.bluebinaries.carrental.R;
+
+public class CarRentalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +23,6 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+
 }
--- app/src/test/java/com/bluebinaries/carrental/ExampleUnitTest.java
+++ app/src/test/java/com/bluebinaries/carrental/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.carrental;
+package com.bluebinaries.carrental;
 
 import org.junit.Test;
 
